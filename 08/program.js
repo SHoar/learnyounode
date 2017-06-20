9a3469d4c3af6831b5bba2bc6caf572859da631b
Sean Hoar <sean.hoar@gmail.com>
2017-06-20 07:33:18 -0400

    Add Official Solution to 08 - HTTP Collect

--- 08/program.js
+++ 08/program.js
@@ -11,3 +11,20 @@ http.get(URL, (response) => {
         console.log(stream.length+"\n"+stream);
     });
 });
+
+/* *** OFFICIAL SOLUTION using 'bl' and pipe function
+var http = require('http')
+var bl = require('bl')
+
+http.get(process.argv[2], function (response) {
+  response.pipe(bl(function (err, data) {
+    if (err) {
+      return console.error(err)
+    }
+    data = data.toString()
+    console.log(data.length)
+    console.log(data)
+  }))
+})
+
+*/
