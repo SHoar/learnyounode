d44b2975a7a69f92bdda0490a6b205d6784efaac
Sean Hoar <sean.hoar@gmail.com>
2017-06-22 19:49:10 -0400

    Overwrite existing TCP server file

--- 10/program.js
+++ 10/program.js
@@ -1,7 +1,20 @@
-const net = require('net')
-const server = net.createServer((socket) => {
-// socket handling logic
-    socket.write(date.getFullYear());
-    socket.end();
-})
-server.listen(8000)
+const PORT = process.argv[2];
+
+const net = require('net');
+let d = new Date();
+
+let year = d.getFullYear();
+let month = d.getMonth();
+let day = d.getDay();
+let hour = d.getHours();
+let minutes = d.getMinutes();
+
+
+const server = net.createServer( (socket) => {
+    socket.end(listener(socket) => {
+        time = `{year}-{month}-{day} {hour}:{minutes}`
+        // YYYY-MM-DD HH:MM 
+        return time;
+    });
+});
+server.listen(PORT)
