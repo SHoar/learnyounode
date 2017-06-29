a3f77db40e36195e583e4bb8409a2d5e48ff1c16
SHoar <sean.hoar@gmail.com>
2017-06-29 18:01:20 -0400

    Start ex 12

---
+++ 12/program.js
@@ -0,0 +1,11 @@
+const http = require('http');
+const map = require('through2-map')
+const port = process.argv[2];
+
+const server = http.createServer((request,response) => {
+   request.pipe(map((chunk) => {
+       return chunk.toString().split('').toUpperCase().join('');
+   })).pipe(response);
+});
+
+server.listen(port);
